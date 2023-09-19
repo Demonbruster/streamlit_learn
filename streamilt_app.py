e716293b33b3131955637837b62a1c14a10f23d9
FM_Suhail <demonbruster@gmail.com>
2023-09-19 13:16:55 +0530

    changes

--- streamilt_app.py
+++ streamilt_app.py
@@ -34,3 +34,9 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("Fruit Load List")
+streamlit.dataframe(my_data_rows)
+
